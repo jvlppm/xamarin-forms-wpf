7a902f3043bbc4c04a88229098c004a553a081ce
Joao Vitor P. Moraes <jvlppm@gmail.com>
2014-07-02 18:52:53 -0300

    Added sample Android project.

---
+++ src/Xamarin.Forms.Platforms/XamlSample.Android/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms;
+using PortableForms;
+
+namespace XamlSample.Android
+{
+    [Activity(Label = "XamlSample.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : AndroidActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            Forms.Init(this, bundle);
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetPage(App.GetMainPage());
+        }
+    }
+}
+
