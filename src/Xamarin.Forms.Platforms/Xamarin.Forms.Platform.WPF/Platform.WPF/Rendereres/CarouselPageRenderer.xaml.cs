f0ab85abd45040ddbb759d5d80d09980cecae5a8
Joao Vitor P. Moraes <jvlppm@gmail.com>
2014-07-05 21:11:34 -0300

    Removed custom item template from FlipView.xaml

--- src/Xamarin.Forms.Platforms/Xamarin.Forms.Platform.WPF/Platform.WPF/Rendereres/CarouselPageRenderer.xaml.cs
+++ src/Xamarin.Forms.Platforms/Xamarin.Forms.Platform.WPF/Platform.WPF/Rendereres/CarouselPageRenderer.xaml.cs
@@ -5,24 +5,33 @@ namespace Xamarin.Forms.Platform.WPF.Rendereres
     using Xamarin.Forms.Platform.WPF.Controls;
     using Xamarin.Forms.Platform.WPF.Converters;
 
-    class CarouselPageRenderer : PageRenderer<CarouselPage, FlipView>
+    /// <summary>
+    /// Interaction logic for CarouselPageRenderer.xaml
+    /// </summary>
+    public partial class CarouselPageRenderer : PageRenderer
     {
+        public new CarouselPage Model
+        {
+            get { return (CarouselPage)base.Model; }
+            set { base.Model = value; }
+        }
+
         public CarouselPageRenderer()
         {
-            Content = new FlipView();
+            InitializeComponent();
             HandleProperty(CarouselPage.ItemsSourceProperty, Handle_ItemsSourceProperty);
             HandleProperty(CarouselPage.SelectedItemProperty, Handle_SelectedItemProperty);
         }
 
         protected virtual bool Handle_ItemsSourceProperty(BindableProperty property)
         {
-            Content.ItemsSource = Model.Children;
+            FlipView.ItemsSource = Model.Children;
             return true;
         }
 
         protected virtual bool Handle_SelectedItemProperty(BindableProperty property)
         {
-            Content.SelectedItem = Model.SelectedItem ?? Model.Children.FirstOrDefault();
+            FlipView.SelectedItem = Model.SelectedItem ?? Model.Children.FirstOrDefault();
             return true;
         }
     }
